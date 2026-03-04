87b63d21391e37c69ea1cdab0898ee777848d745
P Nitish <nitishnaik2022@gmail.com>
2026-03-04 20:59:34 +0530

    fix: auth issues

--- categories.js
+++
@@ -1,6 +0,0 @@
-const url = 'https://backend.composio.dev/api/v3/toolkits/categories';
-const options = {method: 'GET', headers: {'x-api-key': 'ak_2DMBA25XIz4pigtgZvmz'}};
-const response = await fetch(url, options);
-const data = await response.json();
-const categories = new Set(["all", ...data.items.map(item => item.name)])
-console.log(categories)
